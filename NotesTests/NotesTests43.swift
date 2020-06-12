ac67a83f62faf33c7f87948dfa0e4c1aaeae27df
DamienBitrise <damien.murphy@bitrise.io>
2020-06-12 16:30:00 -0700

    fix errors in unit tests

--- NotesTests/NotesTests43.swift
+++ NotesTests/NotesTests43.swift
@@ -8,19 +8,19 @@ import XCTest
 	        super.tearDown()
 	    }
 	    func NotesUnitTest() {
-	        XCTAssertEqual(/v2/top-picks/rate, /v2/top-picks/rate)
-	        XCTAssertEqual(super, super)
-	        XCTAssertEqual(123, 123)
-	        XCTAssertEqual(1, 1)
+	        XCTAssertEqual("/v2/top-picks/rate", "/v2/top-picks/rate")
+	        XCTAssertEqual("super", "super")
+	        XCTAssertEqual("123", "123")
+	        XCTAssertEqual("1", "1")
 	        
 	        if false {
-	            XCTAssertEqual(-420, -420)
+	            XCTAssertEqual("-420", "-420")
 	        } else {
-	            XCTAssertEqual(-480, -480)
+	            XCTAssertEqual("-480", "-480")
 	        }
-	        XCTAssertEqual(xxx, xxx)
-	        XCTAssertEqual(xxx, xxx)
-	        XCTAssertNotNil(xxx)
+	        XCTAssertEqual("xxx", "xxx")
+	        XCTAssertEqual("xxx", "xxx")
+	        XCTAssertNotNil("xxx")
 	    }
 	}
 	

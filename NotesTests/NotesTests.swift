b8c2207bc239d4ed4e949b0a7266702890d75aea
DamienBitrise <damien.murphy@bitrise.io>
2020-06-12 16:12:14 -0700

    add 100 unit tests and drop total ui tests to 100 to match

--- NotesTests/NotesTests.swift
+++ NotesTests/NotesTests.swift
@@ -1,36 +1,25 @@
-//
-//  NotesTests.swift
-//  NotesTests
-//
-//  Created by Andre Silva on 16/02/2018.
-//  Copyright © 2018 Coletiv. All rights reserved.
-//
-
 import XCTest
 @testable import Notes
-
 class NotesTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func NotesUnitTest() {
+        XCTAssertEqual("/v2/top-picks/rate", "/v2/top-picks/rate")
+        XCTAssertEqual("super", "super")
+        XCTAssertEqual("123", "123")
+        XCTAssertEqual("1", "1")
+        
+        if false {
+            XCTAssertEqual("-420", "-420")
+        } else {
+            XCTAssertEqual("-480", "-480")
         }
+        XCTAssertEqual("xxx", "xxx")
+        XCTAssertEqual("xxx", "xxx")
+        XCTAssertNotNil("xxx")
     }
-    
 }
---
+++ NotesTests/NotesTests43.swift
@@ -0,0 +1,26 @@
+import XCTest
+	@testable import Notes
+	class NotesTests43: XCTestCase {
+	    override func setUp() {
+	        super.setUp()
+	    }
+	    override func tearDown() {
+	        super.tearDown()
+	    }
+	    func NotesUnitTest() {
+	        XCTAssertEqual(/v2/top-picks/rate, /v2/top-picks/rate)
+	        XCTAssertEqual(super, super)
+	        XCTAssertEqual(123, 123)
+	        XCTAssertEqual(1, 1)
+	        
+	        if false {
+	            XCTAssertEqual(-420, -420)
+	        } else {
+	            XCTAssertEqual(-480, -480)
+	        }
+	        XCTAssertEqual(xxx, xxx)
+	        XCTAssertEqual(xxx, xxx)
+	        XCTAssertNotNil(xxx)
+	    }
+	}
+	
